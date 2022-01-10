06b4c6b1f2540046a989c1529522d1e2c9b105b1
Luan da Costa <luancostainfo@gmail.com>
2022-01-10 10:07:18 -0300

    Redirecionando usuario para tela de login se o refresh token expirar

--- src/app/shared/servicos/clientes-http-interceptor.service.ts
+++ src/app/shared/servicos/clientes-http-interceptor.service.ts
@@ -3,12 +3,17 @@ import { AuthService } from './auth.service';
 import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { from, mergeMap, Observable } from 'rxjs';
 
+import { Router } from '@angular/router';
+
+export class NotAuthenticatedError {
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesHttpInterceptorService {
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -16,6 +21,10 @@ export class ClientesHttpInterceptorService {
       return from(this.auth.obterNovoAccessToken())
         .pipe(
           mergeMap(() => {
+            if (this.auth.isAccessTokenInvalido()) {
+              this.router.navigateByUrl('/login');
+              throw new NotAuthenticatedError();
+            }
             req = req.clone({
               setHeaders: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
